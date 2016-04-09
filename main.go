32462889c0c1aecca60133bc8d1be7dac363545a
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2016-04-09 16:05:05 +0200

    Support -f n- type flags

--- main.go
+++ main.go
@@ -13,55 +13,82 @@ import (
 var r, f string
 var startEndRe = regexp.MustCompile("^([0-9]+)-([0-9]+)$")
 var startRe = regexp.MustCompile("^([0-9]+)-$")
+var endRe = regexp.MustCompile("^-([0-9+])$")
 
-func parseFieldArgs(f string) (map[uint]bool, error) {
+type OutputConfig struct {
+	Fields                   map[uint]bool
+	HasUnboundedStartingFrom bool
+	UnboundedStartingFrom    uint
+}
+
+func (oc OutputConfig) ShouldOutputField(field uint) bool {
+	if oc.HasUnboundedStartingFrom && field >= oc.UnboundedStartingFrom {
+		return true
+	} else {
+		return oc.Fields[field]
+	}
+}
+
+func createOutputConfig(f string) (OutputConfig, error) {
 	args := strings.Split(strings.TrimSpace(f), ",")
-	var ranges = make(map[uint]bool)
+	var oc = OutputConfig{Fields: make(map[uint]bool)}
 	for _, arg := range args {
 		arg = strings.TrimSpace(arg)
-		if startRe.MatchString(arg) {
+		switch {
+		case startRe.MatchString(arg):
 			matches := startRe.FindStringSubmatch(arg)
 			start, err := strconv.ParseUint(matches[1], 10, 32)
 			if err != nil {
-				return ranges, err
+				return oc, err
 			}
-			ranges[uint(start)] = true
-		} else if startEndRe.MatchString(arg) {
+			oc.HasUnboundedStartingFrom = true
+			oc.UnboundedStartingFrom = uint(start)
+
+		case startEndRe.MatchString(arg):
 			matches := startEndRe.FindStringSubmatch(arg)
 			start, err := strconv.ParseUint(matches[1], 10, 32)
 			if err != nil {
-				return ranges, err
+				return oc, err
 			}
 			end, err := strconv.ParseUint(matches[2], 10, 32)
 			if err != nil {
-				return ranges, err
+				return oc, err
 			}
 
 			for i := uint(start); i <= uint(end); i++ {
-				ranges[i] = true
+				oc.Fields[i] = true
 			}
 
-		} else {
+		case endRe.MatchString(arg):
+			matches := endRe.FindStringSubmatch(arg)
+			end, err := strconv.ParseUint(matches[1], 10, 32)
+			if err != nil {
+				return oc, err
+			}
+			for i := uint(0); i <= uint(end); i++ {
+				oc.Fields[i] = true
+			}
+
+		default:
 			field, err := strconv.ParseUint(arg, 10, 32)
 			if err != nil {
-				return ranges, err
+				return oc, err
 			}
-			ranges[uint(field)] = true
+			oc.Fields[uint(field)] = true
 		}
-
 	}
-	return ranges, nil
+	return oc, nil
 }
 
 func init() {
 	// TODO: long flags http://stackoverflow.com/a/19762274/37208
-	flag.StringVar(&r, "r", "", "Regex to split lines on")
-	flag.StringVar(&f, "f", "1", "Field(s) to output. Default: 1")
+	flag.StringVar(&r, "r", `\s+`, "Regex to split lines on.")
+	flag.StringVar(&f, "f", "1", "Field(s) to output.")
 	flag.Parse()
 }
 
 func main() {
-	ranges, err := parseFieldArgs(f)
+	oc, err := createOutputConfig(f)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Invalid field(s): %s\n", f)
 		os.Exit(1)
@@ -80,7 +107,7 @@ func main() {
 
 		output := make([]string, 0)
 		for i, token := range tokens {
-			if ranges[uint(i+1)] {
+			if oc.ShouldOutputField(uint(i + 1)) {
 				output = append(output, token)
 			}
 		}
@@ -88,7 +115,7 @@ func main() {
 	}
 
 	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		fmt.Fprintln(os.Stderr, "Could not read:", err)
 	}
 
 }
--- main_test.go
+++ main_test.go
@@ -5,40 +5,95 @@ import (
 )
 
 func TestParseFieldArgsHappyDay(t *testing.T) {
-	actual, err := parseFieldArgs("1,2-,3-4,42,1337-1340")
+	param := "1,2-,3-4,42,1337-1340"
+	oc, err := createOutputConfig(param)
 	if err != nil {
-		t.Errorf("parseFieldArgs returned an error: %v", err)
+		t.Errorf("createOutputConfig(%s) returned an error: %v\n", param, err)
 	}
 	for _, field := range []uint{1, 2, 3, 4, 42, 1337, 1338, 1339, 1340} {
-		if !actual[field] {
-			t.Errorf("%v was not in the range as expected", field)
+		if !oc.ShouldOutputField(field) {
+			t.Errorf("-f %s did not include field %v\n", param, field)
 		}
 	}
 }
 
-// func TestParseFieldArgsSingle(t *testing.T) {
-// 	actual, err := parseFieldArgs("1")
-// 	if err != nil {
-// 		t.Errorf("parseFieldArgs returned an error: %v", err)
-// 	}
-// 	expected := []uint64{1}
-// 	if reflect.DeepEqual(actual, expected) {
-// 		t.Errorf("%v did not equal %v", actual, expected)
-// 	}
-// }
+func TestParseFieldArgsSingle(t *testing.T) {
+	param := "1"
+	oc, err := createOutputConfig(param)
+	if err != nil {
+		t.Errorf("createOutputConfig(%s) returned an error: %v\n", param, err)
+	}
+	if !oc.ShouldOutputField(1) {
+		t.Errorf("-f %s did not include field 1\n", param)
+	}
+}
 
-// func TestParseFieldArgsNotAnInt(t *testing.T) {
-// 	bogus := "bogus args"
-// 	_, err := parseFieldArgs(bogus)
-// 	if err == nil {
-// 		t.Errorf("parseFieldArgs didn't return an error with parameter: %v", bogus)
-// 	}
-// }
+func TestParseFieldArgsStart(t *testing.T) {
+	oc, err := createOutputConfig("1-")
+	if err != nil {
+		t.Errorf("createOutputConfig returned an error: %v\n", err)
+	}
+	for i := uint(1); i <= 10; i++ {
+		if !oc.ShouldOutputField(i) {
+			t.Errorf("-f 1- did not include field %d", i)
+		}
+	}
+}
 
-// func TestParseFieldArgsEmpty(t *testing.T) {
-// 	bogus := ""
-// 	_, err := parseFieldArgs(bogus)
-// 	if err == nil {
-// 		t.Error("parseFieldArgs didn't return an error with empty parameter")
-// 	}
-// }
+func TestParseFieldArgsEnd(t *testing.T) {
+	param := "-3"
+	oc, err := createOutputConfig(param)
+	if err != nil {
+		t.Errorf("createOutputConfig(%s) returned an error: %v\n", param, err)
+	}
+
+	for i := uint(1); i <= 3; i++ {
+		if !oc.ShouldOutputField(i) {
+			t.Errorf("-f %s did not include field %d\n", param, i)
+		}
+	}
+	for i := uint(4); i <= 10; i++ {
+		if oc.ShouldOutputField(i) {
+			t.Errorf("-f %s included field %d\n", param, i)
+		}
+	}
+}
+
+func TestParseFieldArgsStartEnd(t *testing.T) {
+	param := "3-5"
+	oc, err := createOutputConfig(param)
+	if err != nil {
+		t.Errorf("createOutputConfig(%s) returned an error: %v\n", param, err)
+	}
+	for i := uint(1); i <= 2; i++ {
+		if oc.ShouldOutputField(i) {
+			t.Errorf("-f %s included field %d\n", param, i)
+		}
+	}
+	for i := uint(3); i <= 5; i++ {
+		if !oc.ShouldOutputField(i) {
+			t.Errorf("-f %s did not include field %d\n", param, i)
+		}
+	}
+	for i := uint(6); i <= 10; i++ {
+		if oc.ShouldOutputField(i) {
+			t.Errorf("-f %s included field %d\n", param, i)
+		}
+	}
+}
+
+func TestParseFieldArgsNotAnInt(t *testing.T) {
+	bogus := "bogus args"
+	_, err := createOutputConfig(bogus)
+	if err == nil {
+		t.Errorf("createOutputConfig didn't return an error with parameter: %v\n", bogus)
+	}
+}
+
+func TestParseFieldArgsEmpty(t *testing.T) {
+	bogus := ""
+	_, err := createOutputConfig(bogus)
+	if err == nil {
+		t.Error("createOutputConfig didn't return an error with empty parameter\n")
+	}
+}
